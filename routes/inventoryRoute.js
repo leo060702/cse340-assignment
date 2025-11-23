b1890bf5a273d206f73926639dff0b36c272525c
leo060702 <zzhong@byupathway.edu>
2025-11-22 17:30:18 -0800

    Add files via upload

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,15 +2,18 @@
 const express = require("express");
 const router = express.Router();
 const inventoryController = require("../controllers/inventoryController");
+const utilities = require("../utilities");
 
-// 分类视图（之前活动已经做过的，如果你函数名不同就改一下）
-router.get("/type/:classificationId",
-  inventoryController.buildByClassificationId
+// 分类视图 /inv/type/:classificationId
+router.get(
+  "/type/:classificationId",
+  utilities.handleErrors(inventoryController.buildByClassificationId)
 );
 
-// 车辆详情页：/inv/detail/1
-router.get("/detail/:invId",
-  inventoryController.buildDetailView
+// 车辆详情页 /inv/detail/:invId
+router.get(
+  "/detail/:invId",
+  utilities.handleErrors(inventoryController.buildDetailView)
 );
 
 module.exports = router;
