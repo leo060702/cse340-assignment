214da6381dc4ad8b7d8447849bc8d899b98fdafe
leo060702 <zzhong@byupathway.edu>
2025-12-09 19:28:38 -0800

    Add listFavorites function to manage vehicle favorites

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -1,9 +1,11 @@
 /**
  * controllers/favoriteController.js
- * Handles add/remove vehicle favorites
+ * Handles add/remove/list vehicle favorites
  */
 
 const favoriteModel = require("../models/favorite-model");
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities");
 
 /**
  * Add favorite
@@ -55,7 +57,51 @@ async function removeFavorite(req, res, next) {
   }
 }
 
+/**
+ * List favorites for current user
+ * GET /favorite
+ */
+async function listFavorites(req, res, next) {
+  try {
+    const nav = await utilities.getNav();
+
+    const accountData = res.locals.accountData;
+    if (!accountData) {
+      req.flash("notice", "Please log in to view your favorites.");
+      return res.redirect("/account/login");
+    }
+
+    const account_id = accountData.account_id;
+
+    // 1. 先从 favorite 表拿到当前用户收藏的 inv_id 列表
+    const favorites = await favoriteModel.getFavoritesByAccountId(account_id);
+
+    // 2. 再根据 inv_id 查询每一辆车的详情
+    const vehicles = [];
+    for (const fav of favorites) {
+      const vehicle = await invModel.getVehicleById(fav.inv_id);
+      if (vehicle) {
+        vehicles.push({
+          ...vehicle,
+          favorite_created_at: fav.created_at,
+        });
+      }
+    }
+
+    return res.render("favorite/list", {
+      title: "My Favorite Vehicles",
+      nav,
+      errors: null,
+      vehicles,
+    });
+  } catch (error) {
+    console.error("listFavorites error:", error);
+    next(error);
+  }
+}
+
 module.exports = {
   addFavorite,
   removeFavorite,
+  listFavorites,
 };
